cf63315f52d73dbad5b2d97c18651a3f8841f0e1
kineticjs <kineticjs@users.noreply.github.com>
2024-07-05 01:06:34 +0000

    Deploying to gh-pages from @ kineticjs/ui5-webcomponents@55d99d25324f64c202339c4947f2b3bcf35e25c8 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1720054968,
+    buildTime: 1720141362,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/ComponentStyles.html-02edf3ba.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-a62479d6.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
