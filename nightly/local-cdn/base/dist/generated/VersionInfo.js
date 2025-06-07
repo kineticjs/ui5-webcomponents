cd8674996a2caf66d9860270e1dda0dfe19e620e
kineticjs <kineticjs@users.noreply.github.com>
2025-06-07 01:30:53 +0000

    Deploying to gh-pages from @ kineticjs/ui5-webcomponents@654306a6e78af70a118afefd24fc4ec4ab15c6e5 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.0",
     isNext: false,
-    buildTime: 1749173253,
+    buildTime: 1749259619,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
