00dbe47c724d9592f37cda8839c35f5ad8550954
kineticjs <kineticjs@users.noreply.github.com>
2025-09-08 01:26:08 +0000

    Deploying to gh-pages from @ kineticjs/ui5-webcomponents@654306a6e78af70a118afefd24fc4ec4ab15c6e5 🚀

---
+++ nightly/assets/test/pages/Icon.html-dd3f2bef.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-123f1a59.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
