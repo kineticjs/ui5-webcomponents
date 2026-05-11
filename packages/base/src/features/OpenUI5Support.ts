e71cb045f22a28250ccc823c2e6eae66434bb108
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2026-05-11 10:33:26 +0300

    fix(OpenUI5Support): css variables detection (#13465)

--- packages/base/src/features/OpenUI5Support.ts
+++ packages/base/src/features/OpenUI5Support.ts
@@ -77,7 +77,7 @@ const OPENUI5_POLLING_INTERVAL = 100;
 class OpenUI5Support {
 	static enablePolling = false; // set to true for old OpenUI5 versions
 
-	static isAtLeastVersion116() {
+	static isAtLeastVersion(minor: number) {
 		if (!window.sap.ui!.version) {
 			return true; // sap.ui.version will be removed in newer OpenUI5 versions
 		}
@@ -86,7 +86,7 @@ class OpenUI5Support {
 		if (!parts || parts.length < 2) {
 			return false;
 		}
-		return parseInt(parts[0]) > 1 || parseInt(parts[1]) >= 116;
+		return parseInt(parts[0]) > 1 || parseInt(parts[1]) >= minor;
 	}
 
 	static isOpenUI5Detected() {
@@ -128,7 +128,7 @@ class OpenUI5Support {
 				window.sap.ui.require(["sap/ui/core/Core"], async (Core: OpenUI5Core) => {
 					const callback = () => {
 						let deps: Array<string> = ["sap/ui/core/Popup", "sap/m/Dialog", "sap/ui/core/Patcher", "sap/ui/core/LocaleData"];
-						if (OpenUI5Support.isAtLeastVersion116()) { // for versions since 1.116.0 and onward, use the modular core
+						if (OpenUI5Support.isAtLeastVersion(116)) { // for versions since 1.116.0 and onward, use the modular core
 							deps = [
 								...deps,
 								"sap/base/i18n/Formatting",
@@ -144,7 +144,7 @@ class OpenUI5Support {
 							resolve();
 						});
 					};
-					if (OpenUI5Support.isAtLeastVersion116()) {
+					if (OpenUI5Support.isAtLeastVersion(116)) {
 						await Core.ready();
 						callback();
 					} else {
@@ -162,7 +162,7 @@ class OpenUI5Support {
 			return {};
 		}
 
-		if (OpenUI5Support.isAtLeastVersion116()) {
+		if (OpenUI5Support.isAtLeastVersion(116)) {
 			const ControlBehavior = window.sap.ui.require("sap/ui/core/ControlBehavior") as ControlBehavior;
 			const Localization = window.sap.ui.require("sap/base/i18n/Localization") as Localization;
 			const Theming = window.sap.ui.require("sap/ui/core/Theming") as Theming;
@@ -211,7 +211,7 @@ class OpenUI5Support {
 
 		const LocaleData = window.sap.ui.require("sap/ui/core/LocaleData") as LocaleData;
 
-		if (OpenUI5Support.isAtLeastVersion116()) {
+		if (OpenUI5Support.isAtLeastVersion(116)) {
 			const Localization = window.sap.ui.require("sap/base/i18n/Localization") as Localization;
 			return LocaleData.getInstance(Localization.getLanguageTag())._get();
 		}
@@ -222,7 +222,7 @@ class OpenUI5Support {
 	}
 
 	static _listenForThemeChange() {
-		if (OpenUI5Support.isAtLeastVersion116()) {
+		if (OpenUI5Support.isAtLeastVersion(116)) {
 			const Theming: Theming = window.sap.ui.require("sap/ui/core/Theming");
 			Theming.attachApplied(() => {
 				setTheme(Theming.getTheme());
@@ -256,7 +256,11 @@ class OpenUI5Support {
 		}
 
 		// The file name is "css_variables.css" until 1.127 and "library.css" from 1.127 onwards
-		return !!link.href.match(/\/css(-|_)variables\.css/) || !!link.href.match(/\/library\.css/);
+		if (OpenUI5Support.isAtLeastVersion(127)) {
+			return !!link.href.match(/\/css(-|_)variables\.css/) || !!link.href.match(/\/library\.css/);
+		}
+
+		return !!link.href.match(/\/css(-|_)variables\.css/);
 	}
 
 	static addOpenedPopup(popupInfo: PopupInfo) {
